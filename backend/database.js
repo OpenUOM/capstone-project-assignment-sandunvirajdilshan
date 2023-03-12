a9cedc882b40b2a522e2eeaa02e788056cf73f48
sandunvirajdilshan <sandunvirajdilshan@gmail.com>
2023-03-12 21:15:24 +0530

    backend tasks

--- backend/database.js
+++ backend/database.js
@@ -18,8 +18,18 @@ function init(db) {
 
 const knex_db = require("./db-config");
 
-const dbinitialize = async () => {
-    testBase.resetDatabase(knex_db);
+const addTeacher = async (id, name, age) => {
+    const sql = `INSERT INTO teacher(id,name,age) values (?, ?, ?)`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql, [id, name, age])
+            .then(() => {
+                resolve({status: "Successfully inserted Teacher"})
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 const readTeachers = async () => {
@@ -43,20 +53,6 @@ const readTeacherInfo = async (id) => {
             .raw(sql, [id])
             .then((teacher) => {
                 resolve(teacher);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
-const addTeacher = async (id, name, age) => {
-    const sql = `INSERT INTO teacher(id,name,age) values (?, ?, ?)`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql, [id, name, age])
-            .then(() => {
-                resolve({status: "Successfully inserted Teacher"})
             })
             .catch((error) => {
                 reject(error);
@@ -92,85 +88,10 @@ const deleteTeacher = async (id) => {
     });
 }
 
-const readStudents = async () => {
-    const sql = `SELECT * FROM student`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql)
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
-const readStudentInfo = async (id) => {
-    const sql = `SELECT * FROM student WHERE id = ?`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql,[id])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
-const addStudent = async (id, name, age, hometown) => {
-    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?, ?)`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql,[id, name, age, hometown])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
-const updateStudent = async (name, age, hometown, id) => {
-    const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql,[name, age, hometown, id])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-} 
-
-const deleteStudent = async (id) => {
-    const sql = `DELETE FROM student WHERE id = ?`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql,[id])
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
 module.exports = {
-    readTeachers,
-    readStudents,
-    addStudent,
     addTeacher,
-    deleteTeacher,
-    deleteStudent,
-    readStudentInfo,
+    readTeachers,
     readTeacherInfo,
-    updateStudent,
-    updateTeacher
-};
+    updateTeacher,
+    deleteTeacher
+}
--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,11 @@ app.get("/dbinitialize", async function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
-// ============== Teacher Related endpoints ==============
 
 app.get("/listTeachers", async function (req, res) {
   console.log("Request received to list teachers");
   let data = await readTeachers();
-
+  
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
@@ -48,21 +47,10 @@ app.post("/getTeacherInfo", async function (req, res) {
 app.post("/addTeacher", async function (req, res) {
   let reqBody = req.body;
   console.log(
-    "Request received to add teacher. Req body: " + JSON.stringify(reqBody)
+  "Request received to add teacher. Req body: " + JSON.stringify(reqBody)
   );
   let data = await addTeacher(reqBody.id, reqBody.name, reqBody.age);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/editTeacher", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to update teacher. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await updateTeacher(reqBody.name,reqBody.age,reqBody.id);
-
+  
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
@@ -70,66 +58,17 @@ app.post("/editTeacher", async function (req, res) {
 app.post("/deleteTeacher", async function (req, res) {
   let reqBody = req.body;
   console.log(
-    "Request received to delete teacher. Req body: " + JSON.stringify(reqBody)
+  "Request received to delete teacher. Req body: " + JSON.stringify(reqBody)
   );
   let data = await deleteTeacher(reqBody.id);
-
+  
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
 
-// ============== Student Related endpoints ==============
-
-app.get("/listStudents", async function (req, res) {
-  console.log("Request received to list students");
-  let data = await readStudents();
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/getStudentInfo", async function (req, res) {
+app.post("/editTeacher", async function (req, res) {
   let reqBody = req.body;
-  console.log("Request received to get Student Info");
-  let data = await readStudentInfo(reqBody.id);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/addStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to add student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await addStudent(
-    reqBody.id,
-    reqBody.name,
-    reqBody.age,
-    reqBody.hometown
-  );
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/deleteStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to delete student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await deleteStudent(reqBody.id);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/editStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to update Student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
+  let data = await updateTeacher(reqBody.name, reqBody.age, reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
